b1f58a41cc6d5fb5b64a9ecd648cee3be6f96c98
gerry <gerald.hofbauer@icloud.com>
2019-08-06 01:59:23 +0200

    Behebt Linting-Probleme wegen einfacher Anführungszeichen

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDSDjZvLdWEhsdSkwti_-fnEvrahyMMr_s",
-    authDomain: "filesharingapp-vwa-dev.firebaseapp.com",
-    databaseURL: "https://filesharingapp-vwa-dev.firebaseio.com",
-    projectId: "filesharingapp-vwa-dev",
-    storageBucket: "filesharingapp-vwa-dev.appspot.com",
-    messagingSenderId: "872979305527",
-    appId: "1:872979305527:web:9ebf40d5dc7b3b9c"
-  }
+    apiKey: 'AIzaSyDSDjZvLdWEhsdSkwti_-fnEvrahyMMr_s',
+    authDomain: 'filesharingapp-vwa-dev.firebaseapp.com',
+    databaseURL: 'https://filesharingapp-vwa-dev.firebaseio.com',
+    projectId: 'filesharingapp-vwa-dev',
+    storageBucket: 'filesharingapp-vwa-dev.appspot.com',
+    messagingSenderId: '872979305527',
+    appId: '1:872979305527:web:9ebf40d5dc7b3b9c',
+  },
 };
 
 /*
