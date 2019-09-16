be6b81200ac7a7631896b18592ca726e259fc352
gerry <gerald.hofbauer@icloud.com>
2019-09-16 12:41:43 +0200

    Erstellt einen FileService, der zur Komprimierung und dem Teilen von Dateien dient und Icons für Dateien anhand der Endung ermittelt

---
+++ src/app/services/file.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FileService } from './file.service';
+import { flatMap, tap } from 'rxjs/operators';
+import { from } from 'rxjs';
+
+describe('FileService', () => {
+  let service: FileService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.get(FileService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should compress a text blob', (done) => {
+    const testBlob = new Blob([ new TextEncoder().encode('TestFile').buffer ], { type: 'text/plain' });
+    service.compress(testBlob).pipe(
+      flatMap(service.decompress),
+    ).subscribe((blob) => {
+      expect(blob.size).toEqual(testBlob.size);
+      expect(blob.type).toEqual(testBlob.type);
+      done();
+    }, done.fail);
+  });
+
+  it('should compress and decompress a image blob', (done) => {
+    let testBlob: Blob;
+    from(
+      fetch('https://picsum.photos/1272/708'),
+    ).pipe(
+      flatMap((res) => res.blob()),
+      tap((blob) => testBlob = blob),
+      flatMap(service.compress),
+      flatMap(service.decompress),
+    ).subscribe((blob) => {
+      expect(blob.size).toEqual(testBlob.size);
+      expect(blob.type).toEqual(testBlob.type);
+      done();
+    }, done.fail);
+  });
+
+  it('should get an icon URL for a PNG image', () => {
+    expect(service.getFileIconURL('test.image.png')).toEqual('assets/file-icons/PNG.svg');
+  });
+
+  it('should get an icon URL for an unknown file type', () => {
+    expect(service.getFileIconURL('test.unknown.xxx')).toEqual('assets/file-icons/Unknown.svg');
+  });
+
+});
