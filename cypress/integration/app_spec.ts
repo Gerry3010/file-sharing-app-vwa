e95823966a2d3a6e08b49acdb98521f6eba04571
gerry <gerald.hofbauer@icloud.com>
2020-04-27 23:23:07 +0200

    Initial commit

--- cypress/integration/app_spec.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="cypress" />
-
-
-describe('FileSharingApp', () => {
-  it('should load', () => {
-    cy.visit('http://localhost:4200');
-  });
-});
--- src/app/effects/index.ts
+++
@@ -1,7 +0,0 @@
-import { AppEffects } from './app.effects';
-import { FileRequestEffects } from './file-request.effects';
-
-export const effects = [
-  AppEffects,
-  FileRequestEffects,
-];
